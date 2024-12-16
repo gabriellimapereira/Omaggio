fae824dff15592133bdce575baf110f26255a749
gabriellimapereira <gaab.ollim@gmail.com>
2024-12-16 13:29:44 -0300

    rascunho das estruturas das listas de tarefas

--- functions.h
+++ functions.h
@@ -1,10 +1,47 @@
 #ifndef FUNCTIONS
 #define FUNCTIONS
 
+typedef struct Task {
+    char name[50];
+    char deadline[10];
+    int priority;
+} Task;
+
+typedef struct TaskList { //lista encadeada
+    struct Task* task;
+    struct Task* next;
+} TaskList;
+
+typedef struct CompletedTasks { //lista encadeada-circular
+    struct Task* task;
+    struct Task* next;
+} CompletedTasks;
+
+typedef struct DoublyTaskList { //lista duplamente-encadeada
+    struct Task* task;
+    struct Task* next;
+    struct Task* prev;
+} DoublyTaskList;
+
+typedef struct PendingTasks { //tarefas pendentes
+    struct Task* first;
+    struct Task* last;
+} PendingTasks;
+
+typedef struct UndoStack { //pilha para reverter ações
+    struct User* top;
+} UndoStack;
+
 typedef struct User {
+    char name[50];
     int id;
     struct User* next;
     struct User* prev;
+    struct TaskList* taskList;
+    struct CompletedTasks* completedList;
+    struct DoublyTaskList* doublyTaskList;
+    struct PendingStask* pendingList;
+    struct UndoStack* history;
 } User;
 
 User* allocateUser(int newId);
